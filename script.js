ba15bec5f2577105c700559b92194a7ce41131bd
ViRack <vincentm_2@hotmail.com>
2024-12-08 23:45:02 -0800

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const header = document.querySelector(".header");
+const nav = document.querySelector('.nav');
+
 
 const openModal = function (e) {
   e.preventDefault();
@@ -122,3 +124,23 @@ tabsContainer.addEventListener("click", function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add("operations__content--active");
 });
+
+
+// menu fade animation component
+
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    })
+    logo.style.opacity = this;
+  }
+}
+
+// passing "argument" into handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
